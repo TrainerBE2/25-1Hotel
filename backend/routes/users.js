b30a08190e99dab951fc2a6bb4c59c68e287af2d
Akbardavid <akbardavid034@gmail.com>
2024-06-20 07:28:08 +0700

    Fixing route: 1

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -13,7 +13,7 @@ const authMidlleware = require("../middleware/authMiddleware");
 /* 
   Root Level
 */
-router.get("/", getAllUsers);
+router.get("/", authMidlleware(["admin", "users"]), getAllUsers);
 router.put("/:id", authMidlleware(["root"]), deleteUsers);
 router.put("/:id", authMidlleware(["root"]), restoreUsers);
 router.get("/:id", getUserById);
